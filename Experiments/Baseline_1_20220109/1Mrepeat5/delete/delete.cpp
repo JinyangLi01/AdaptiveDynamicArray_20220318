12a9d03b8fc43f356cbba1ee4a5418903f2ecdb2
JinyangLi01 <jinyli@umich.edu>
2022-04-02 02:13:11 -0400

    Update delete.cpp

--- Experiments/Baseline_1_20220109/1Mrepeat5/delete/delete.cpp
+++ Experiments/Baseline_1_20220109/1Mrepeat5/delete/delete.cpp
@@ -364,6 +364,7 @@ int main(int argc, char** argv) {
         finstant << fl << ",";
 
         int pos = RandomInt(1, NowTotalNum);
+        printf("delete %d\n", pos);
 
         time1 = timeNow();
         SADelete(standard_array, pos, NowTotalNum);
