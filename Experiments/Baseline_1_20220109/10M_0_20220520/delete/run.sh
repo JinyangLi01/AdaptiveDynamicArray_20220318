f6be471b35c9c4f8ec5ca4fd2874226068798bc6
JinyangLi01 <jinyli@umich.edu>
2022-05-21 04:54:01 -0400

    10M no repeat

---
+++ Experiments/Baseline_1_20220109/10M_0_20220520/delete/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+make
+./delete/delete delete1.csv delete1Log.txt
+#./delete/delete delete2.csv delete2Log.txt
+#./delete/delete delete3.csv delete3Log.txt
+#./delete/delete delete4.csv delete4Log.txt
+#./delete/delete delete5.csv delete5Log.txt
+./delete/average delete
+python3 ./delete/fig.py
---
+++ Experiments/Baseline_1_20220109/10M_0_20220520/reorder/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+make
+./reorder/reorder reorder1.csv reorder1Log.txt
+#./reorder/reorder reorder2.csv reorder2Log.txt
+#./reorder/reorder reorder3.csv reorder3Log.txt
+#./reorder/reorder reorder4.csv reorder4Log.txt
+#./reorder/reorder reorder5.csv reorder5Log.txt
+./reorder/average reorder
+python3 ./reorder/fig.py
