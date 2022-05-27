3f2196e4a2c15665f02f5e198ff0eec9b3ec50a7
JinyangLi01 <jinyli@umich.edu>
2022-05-27 17:43:14 -0400

    1M repeat5

--- Experiments/Baseline_1_20220109/1Mrepeat5/fig.py
+++ Experiments/Baseline_1_20220109/1Mrepeat5/fig.py
@@ -15,20 +15,21 @@ sns.set_style("whitegrid")
 # sns.palplot(sns.color_palette("Paired", 9))
 
 line_style = ['o-', 's--', '^:', '-.p', 'h-', 'D--']
-color = ['C5', 'C1', 'C2', 'C3', 'C4', 'C0']
-plt_title = ["BlueNile", "COMPAS", "Credit Card"]
+color = ['C5', 'C1', 'C7', 'C3', 'C9']
+# color = ['red', 'blue', 'green', 'orange', 'black']
+
+# plt_title = ["BlueNile", "COMPAS", "Credit Card"]
 
 label = ["ADA", "SA", "LL", "TV", "VEC"]
 line_width = 8
 marker_size = 15
-# f_size = (14, 10)
 
 f_size = (14, 10)
 
 
 
-def hundreds_formatter(x, pos):
-    return int(x/100)
+# def hundreds_formatter(x, pos):
+#     return int(x/100)
 
 
 
@@ -43,7 +44,7 @@ execution_time5 = list()
 execution_time6 = list()
 
 
-input_path = r'swap_avg.csv'
+input_path = r'delete_avg.csv'
 input_file = open(input_path, "r")
 
 
@@ -90,16 +91,17 @@ for i in range(0, 5):
 #          markersize=marker_size)
 plt.xscale('log')
 plt.xlabel("\%")
-plt.xticks(ticks=[0.0001, 0.001, 0.01, 0.1, 1], labels=[0.001, 0.01, 1, 10, 100])
-# ax.xaxis.set_major_formatter(FuncFormatter(hundreds_formatter))
+plt.xticks(ticks=[0.00001, 0.0001, 0.001, 0.01, 0.1, 1], labels=[0.001, 0.01, 0.1, 1, 10, 100])
+
 plt.ylabel('µs')
 plt.yscale('log')
-# plt.yticks(ticks=[100, 1000, 10000, 100000, 1000000])
+# plt.yticks(ticks=[100, 1000])
+
 plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=6, fontsize=22)
 
 plt.grid(True)
 fig.tight_layout()
-plt.savefig("swap.png",
+plt.savefig("delete.png",
             bbox_inches='tight')
 plt.show()
 plt.close()
