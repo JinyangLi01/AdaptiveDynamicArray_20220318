b7def42b4761fc3ecace65721b5e92e684d0d2f7
JinyangLi01 <jinyli@umich.edu>
2022-05-28 00:45:05 -0400

    scalability 1M

--- Experiments/Scalability_3/1M/insert/fig.py
+++ Experiments/Scalability_3/1M/insert/fig.py
@@ -2,13 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import colorsys
 from matplotlib.ticker import FuncFormatter
-def scale_lightness(rgb, scale_l):
-    # convert rgb to hls
-    h, l, s = colorsys.rgb_to_hls(*rgb)
-    # manipulate h, l, s values and return as rgb
-    return colorsys.hls_to_rgb(h, min(1, l * scale_l), s=s)
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
@@ -20,15 +14,11 @@ sns.set_style("whitegrid")
 # sns.palplot(sns.color_palette("deep", 10))
 # sns.palplot(sns.color_palette("Paired", 9))
 
-line_style = ['o-', 'o-', 'o-', 's--', 's--', 's--', '^:', '^:', '^:',
-              '-.p', '-.p', '-.p', 'h-', 'h-', 'h-', 'D--', 'D--', 'D--']
-# color = ['C5', 'C1', 'C2', 'C3', 'C4', 'C0']
-color = ['red', 'red', 'red', 'blue', 'blue', 'blue', 'green', 'green', 'green',
-         'purple', 'purple', 'purple', 'gold', 'gold', 'gold', 'grey', 'grey', 'grey']
-label = ["ADA", "ADA", "ADA", "SA", "SA", "SA", "LL", "LL", "LL",
-         "CBT", "CBT", "CBT", "TV", "TV", "TV", "VEC", "VEC", "VEC"]
-alphas = [1, 0.6, 0.3, 1, 0.6, 0.3, 1, 0.6, 0.3, 1, 0.6, 0.3, 1, 0.6, 0.3, 1, 0.6, 0.3]
+line_style = ['o-', 's--', '^:', '-.p', 'h-', 'D--']
+color = ['C5', 'C1', 'C2', 'C3', 'C4', 'C0']
+plt_title = ["BlueNile", "COMPAS", "Credit Card"]
 
+label = ["ADA", "SA", "LL", "CBT", "TV", "VEC"]
 line_width = 8
 marker_size = 15
 # f_size = (14, 10)
@@ -53,7 +43,7 @@ execution_time5 = list()
 execution_time6 = list()
 
 
-input_path = r'insert.csv'
+input_path = r'avg.csv'
 input_file = open(input_path, "r")
 
 
@@ -66,22 +56,40 @@ for line in Lines:
     count += 1
     if line == '\n':
         break
-    if count < 1:
+    if count < 2:
         continue
+
     items = line.strip().split(',')
     x_list.append(float(items[0]))
-    for i in range(0, 18):
+    for i in range(0, 6):
         execution_time[i].append(float(items[i+1]))
+    # execution_time1.append(float(items[1]))
+    # execution_time2.append(float(items[2]))
+    # execution_time3.append(float(items[3]))
+    # execution_time4.append(float(items[4]))
+    # execution_time5.append(float(items[5]))
+    # execution_time6.append(float(items[6]))
 
 
 
 
 fig, ax = plt.subplots(1, 1, figsize=f_size)
-for i in range(0, 18):
-    plt.plot(x_list, execution_time[i], color=color[i], label=label[i], alpha=alphas[i], linewidth=line_width,
+for i in range(0, 6):
+    plt.plot(x_list, execution_time[i], color=color[i], label=label[i], linewidth=line_width,
          markersize=marker_size)
-# plt.xscale('log')
-# plt.xticks(ticks=[0.0001, 0.001, 0.01, 0.1, 1], labels=[0.001, 0.01, 1, 10, 100])
+
+# plt.plot(x_list, execution_time2, line_style[1], color=color[1], label=label[1], linewidth=line_width,
+#          markersize=marker_size)
+# plt.plot(x_list, execution_time1, line_style[0], color=color[0], label=label[0], linewidth=line_width,
+#          markersize=marker_size)
+# plt.plot(x_list, execution_time2, line_style[1], color=color[1], label=label[1], linewidth=line_width,
+#          markersize=marker_size)
+# plt.plot(x_list, execution_time1, line_style[0], color=color[0], label=label[0], linewidth=line_width,
+#          markersize=marker_size)
+# plt.plot(x_list, execution_time2, line_style[1], color=color[1], label=label[1], linewidth=line_width,
+#          markersize=marker_size)
+plt.xscale('log')
+plt.xticks(ticks=[0.0001, 0.001, 0.01, 0.1, 1], labels=[0.001, 0.01, 1, 10, 100])
 ax.set_xlabel(" \% ")
 # ax.xaxis.set_major_formatter(FuncFormatter(hundreds_formatter))
 plt.ylabel('µs')
---
+++ Experiments/Scalability_3/1M/swap/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+make
+./swap/swap swap/swap1.csv swap/swap1Log.txt
+./swap/swap swap/swap2.csv swap/swap2Log.txt
+./swap/swap swap/swap3.csv swap/swap3Log.txt
+./swap/swap swap/swap4.csv swap/swap4Log.txt
+./swap/swap swap/swap5.csv swap/swap5Log.txt
+./swap/average swap
+python3 ./swap/fig.py
