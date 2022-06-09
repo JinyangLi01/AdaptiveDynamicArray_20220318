cce72a57feb02065241c478724f40cea0b542bf9
JinyangLi01 <jinyli@umich.edu>
2022-06-09 16:52:11 +0000

    1M repeat5

--- Experiments/Baseline_1_20220109/1Mrepeat5/pointquery/run.sh
+++ Experiments/Baseline_1_20220109/1Mrepeat5/pointquery/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 make
 # ./pointquery avg.csv pointqueryLog.txt
-./pointquery/pointquery pointquery1.csv pointquery1Log.txt
-./pointquery/pointquery pointquery2.csv pointquery2Log.txt
-./pointquery/pointquery pointquery3.csv pointquery3Log.txt
-./pointquery/pointquery pointquery4.csv pointquery4Log.txt
-./pointquery/pointquery pointquery5.csv pointquery5Log.txt
+./pointquery/pointquery pointquery/pointquery1.csv pointquery/pointquery1Log.txt
+./pointquery/pointquery pointquery/pointquery2.csv pointquery/pointquery2Log.txt
+./pointquery/pointquery pointquery/pointquery3.csv pointquery/pointquery3Log.txt
+./pointquery/pointquery pointquery/pointquery4.csv pointquery/pointquery4Log.txt
+./pointquery/pointquery pointquery/pointquery5.csv pointquery/pointquery5Log.txt
 ./pointquery/average pointquery
-python3 ./pointquery/fig.py
+
--- Experiments/Baseline_1_20220109/1Mrepeat5/query/run.sh
+++ Experiments/Baseline_1_20220109/1Mrepeat5/query/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 make
-./query/query query1.csv query1Log.txt
-./query/query query2.csv query2Log.txt
-./query/query query3.csv query3Log.txt
-./query/query query4.csv query4Log.txt
-./query/query query5.csv query5Log.txt
+./query/query query/query1.csv query/query1Log.txt
+./query/query query/query2.csv query/query2Log.txt
+./query/query query/query3.csv query/query3Log.txt
+./query/query query/query4.csv query/query4Log.txt
+./query/query query/query5.csv query/query5Log.txt
 ./query/average query
-python3 ./query/fig.py
+
