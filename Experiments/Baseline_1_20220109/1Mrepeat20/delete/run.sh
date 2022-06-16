cd092287c11ca102b937b2811796f51505087e90
JinyangLi01 <jinyli@umich.edu>
2022-06-16 06:38:22 +0000

    1M repeat20

--- Experiments/Baseline_1_20220109/1Mrepeat20/delete/run.sh
+++ Experiments/Baseline_1_20220109/1Mrepeat20/delete/run.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 make
-./delete/delete delete/delete1.csv delete/delete1Log.txt
-./delete/delete delete/delete2.csv delete/delete2Log.txt
-./delete/delete delete/delete3.csv delete/delete3Log.txt
-./delete/delete delete/delete4.csv delete/delete4Log.txt
-./delete/delete delete/delete5.csv delete/delete5Log.txt
+./delete/delete delete/delete6.csv delete/delete6Log.txt
+./delete/delete delete/delete7.csv delete/delete7Log.txt
+./delete/delete delete/delete8.csv delete/delete8Log.txt
+./delete/delete delete/delete9.csv delete/delete9Log.txt
+./delete/delete delete/delete10.csv delete/delete10Log.txt
+./delete/delete delete/delete11.csv delete/delete11Log.txt
+./delete/delete delete/delete12.csv delete/delete12Log.txt
+./delete/delete delete/delete13.csv delete/delete13Log.txt
+./delete/delete delete/delete14.csv delete/delete14Log.txt
+./delete/delete delete/delete15.csv delete/delete15Log.txt
+./delete/delete delete/delete16.csv delete/delete16Log.txt
+./delete/delete delete/delete17.csv delete/delete17Log.txt
+./delete/delete delete/delete18.csv delete/delete18Log.txt
+./delete/delete delete/delete19.csv delete/delete19Log.txt
+./delete/delete delete/delete20.csv delete/delete20Log.txt
 ./delete/average delete
--- Experiments/Baseline_1_20220109/1Mrepeat20/pointquery/run.sh
+++ Experiments/Baseline_1_20220109/1Mrepeat20/pointquery/run.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 make
-# ./pointquery avg.csv pointqueryLog.txt
-./pointquery/pointquery pointquery/pointquery1.csv pointquery/pointquery1Log.txt
-./pointquery/pointquery pointquery/pointquery2.csv pointquery/pointquery2Log.txt
-./pointquery/pointquery pointquery/pointquery3.csv pointquery/pointquery3Log.txt
-./pointquery/pointquery pointquery/pointquery4.csv pointquery/pointquery4Log.txt
-./pointquery/pointquery pointquery/pointquery5.csv pointquery/pointquery5Log.txt
+./pointquery/pointquery pointquery/pointquery6.csv pointquery/pointquery6Log.txt
+./pointquery/pointquery pointquery/pointquery7.csv pointquery/pointquery7Log.txt
+./pointquery/pointquery pointquery/pointquery8.csv pointquery/pointquery8Log.txt
+./pointquery/pointquery pointquery/pointquery9.csv pointquery/pointquery9Log.txt
+./pointquery/pointquery pointquery/pointquery10.csv pointquery/pointquery10Log.txt
+./pointquery/pointquery pointquery/pointquery11.csv pointquery/pointquery11Log.txt
+./pointquery/pointquery pointquery/pointquery12.csv pointquery/pointquery12Log.txt
+./pointquery/pointquery pointquery/pointquery13.csv pointquery/pointquery13Log.txt
+./pointquery/pointquery pointquery/pointquery14.csv pointquery/pointquery14Log.txt
+./pointquery/pointquery pointquery/pointquery15.csv pointquery/pointquery15Log.txt
+./pointquery/pointquery pointquery/pointquery16.csv pointquery/pointquery16Log.txt
+./pointquery/pointquery pointquery/pointquery17.csv pointquery/pointquery17Log.txt
+./pointquery/pointquery pointquery/pointquery18.csv pointquery/pointquery18Log.txt
+./pointquery/pointquery pointquery/pointquery19.csv pointquery/pointquery19Log.txt
+./pointquery/pointquery pointquery/pointquery20.csv pointquery/pointquery20Log.txt
 ./pointquery/average pointquery
-
