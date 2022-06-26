3d548c6701250034af750255d02233f6a2dc7de1
JinyangLi01 <jinyli@umich.edu>
2022-06-26 17:13:50 -0400

    10M repeat 5 fix average

--- Experiments/Scalability_3/10M/pointquery/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-make
-./pointquery/pointquery pointquery/pointquery1.csv pointquery/pointquery1Log.txt
-# ./pointquery/pointquery pointquery/pointquery2.csv pointquery/pointquery2Log.txt
-# ./pointquery/pointquery pointquery/pointquery3.csv pointquery/pointquery3Log.txt
-# ./pointquery/pointquery pointquery/pointquery4.csv pointquery/pointquery4Log.txt
-# ./pointquery/pointquery pointquery/pointquery5.csv pointquery/pointquery5Log.txt
-#./pointquery/average pointquery
