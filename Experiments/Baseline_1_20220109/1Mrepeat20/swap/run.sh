116ca28bbe3859b65437768a6b9fb9ff05750fc1
JinyangLi01 <jinyli@umich.edu>
2022-07-14 01:45:58 +0000

    1M repeat20

--- Experiments/Baseline_1_20220109/1Mrepeat20/swap/run.sh
+++ Experiments/Baseline_1_20220109/1Mrepeat20/swap/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 make
+./swap/swap swap/swap1.csv swap/swap1Log.txt
+./swap/swap swap/swap2.csv swap/swap2Log.txt
+./swap/swap swap/swap3.csv swap/swap3Log.txt
+./swap/swap swap/swap4.csv swap/swap4Log.txt
+./swap/swap swap/swap5.csv swap/swap5Log.txt
 ./swap/swap swap/swap6.csv swap/swap6Log.txt
 ./swap/swap swap/swap7.csv swap/swap7Log.txt
 ./swap/swap swap/swap8.csv swap/swap8Log.txt
